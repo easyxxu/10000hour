30237a2f3456fba1de169bb7bb9b028d655385b7
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-05-09 23:54:02 +0900

    📝 모달창, 공유하기 버튼 이벤트 추가

--- src/components/GoTraining/GoTraining.jsx
+++ src/components/GoTraining/GoTraining.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+import Modal from "../Modal/Modal";
 import "./GoTraining.css";
 export default function GoTraining() {
+  const [modal, setModal] = useState(false);
+  function modalView() {
+    setModal(true);
+  }
+  function modalClose() {
+    setModal(false);
+  }
+  function btnShare() {
+    alert("링크가 복사되었습니다!");
+  }
   return (
     <section className="section-training">
-      <button className="btn-training">훈련하러 가기 GO!GO!</button>
-      <button className="btn-share">공유하기</button>
+      <button className="btn-training" onClick={modalView}>
+        훈련하러 가기 GO!GO!
+      </button>
+      <button className="btn-share" onClick={btnShare}>
+        공유하기
+      </button>
+      {modal && <Modal modalClose={modalClose} />}
     </section>
   );
 }

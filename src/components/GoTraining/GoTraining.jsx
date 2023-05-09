d21c00f52be7550c3a1193d6d02fb72287f950f7
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-05-09 22:40:17 +0900

    📝 GoTraining 컴포넌트

---
+++ src/components/GoTraining/GoTraining.jsx
@@ -0,0 +1,9 @@
+import "./GoTraining.css";
+export default function GoTraining() {
+  return (
+    <section className="section-training">
+      <button className="btn-training">훈련하러 가기 GO!GO!</button>
+      <button className="btn-share">공유하기</button>
+    </section>
+  );
+}

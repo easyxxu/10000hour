4eb15b697ad6569c48522d1ebf2b06aad32b035e
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-05-10 16:59:48 +0900

    📝 결과창 수정중

--- src/components/MyInfo/MyInfo.jsx
+++ src/components/MyInfo/MyInfo.jsx
@@ -1,45 +1,51 @@
 // import { useState } from "react";
 import "./MyInfo.css";
 import click from "../../img/click.png";
+
 export default function MyInfo({ expert, time, input, result }) {
-  const btnCount = (e) => {
+  function btnCount(e) {
+    e.preventDefault();
     input(true);
     result(false);
     setTimeout(() => {
       input(false);
       result(true);
     }, 4000);
-  };
-  const changeExpert = (e) => {
-    // console.log("expert : ", e.target.value);
-    expert(e.target.value);
-  };
-  const changeTime = (e) => {
-    time(Math.floor(10000 / e.target.value));
-    // console.log("time : ", Math.floor(10000 / e.target.value));
-  };
+  }
+
   return (
     <section className="section-myInfo">
-      <div className="wrap-ihaveto">
-        <p>나는</p>
-        <input
-          type="text"
-          placeholder="예)프로그래밍"
-          onChange={changeExpert}
-        />
-        <p>전문가가 될 것 이다.</p>
-      </div>
-      <div className="wrap-ihaveto">
-        <p>그래서 앞으로 매일 하루에</p>
-        <input type="number" placeholder="예)5" onChange={changeTime} />
-        <p>시간씩 훈련할 것이다.</p>
-      </div>
-      <div className="btn-box">
-        <button className="btn-cnt" onClick={btnCount}>
-          나는 며칠 동안 훈련을 해야 1만 시간이 될까?
-        </button>
-        <img src={click} alt="click" />
-      </div>
+      <form onSubmit={btnCount}>
+        <label>
+          <span>나는</span>
+          <input
+            type="text"
+            placeholder="예)프로그래밍"
+            onChange={(e) => {
+              expert(e.target.value);
+            }}
+          />
+          <span>전문가가 될 것 이다.</span>
+        </label>
+
+        <label>
+          <span>그래서 앞으로 매일 하루에</span>
+          <input
+            type="number"
+            placeholder="예)5"
+            onChange={(e) => {
+              time(Math.floor(10000 / e.target.value));
+            }}
+          />
+          <span>시간씩 훈련할 것이다.</span>
+        </label>
+        <div className="btn-box">
+          <button type="submit" className="btn-submit">
+            나는 며칠 동안 훈련을 해야 1만 시간이 될까?
+          </button>
+          <img src={click} alt="click" />
+        </div>
+      </form>
     </section>
   );
 }

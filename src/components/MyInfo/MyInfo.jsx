f28b20a384779255dc191f36fbc9294ae5d6e358
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-05-10 21:37:06 +0900

    📝 submit 버튼을 눌러야 결과창에 반영되도록 변경

--- src/components/MyInfo/MyInfo.jsx
+++ src/components/MyInfo/MyInfo.jsx
@@ -1,29 +1,36 @@
 // import { useState } from "react";
 import "./MyInfo.css";
 import click from "../../img/click.png";
+import { useState } from "react";
 
-export default function MyInfo({ expert, time, input, result }) {
-  function btnCount(e) {
+export default function MyInfo({ setExpert, setTime, setInput, setResult }) {
+  const [timeval, setTimeval] = useState("");
+  const [expertval, setExpertval] = useState("");
+
+  function Count(e) {
     e.preventDefault();
-    input(true);
-    result(false);
+    setTime(Math.floor(10000 / timeval));
+    setExpert(expertval);
+    setInput(true); // 로딩 컴포넌트 출력
+    setResult(false); // submit을 또 누른 경우, 결과창 안보이도록 함
     setTimeout(() => {
-      input(false);
-      result(true);
-    }, 4000);
+      setInput(false);
+      setResult(true);
+    }, 3000);
   }
 
   return (
     <section className="section-myInfo">
-      <form onSubmit={btnCount}>
+      <form onSubmit={Count}>
         <label>
           <span>나는</span>
           <input
             type="text"
             placeholder="예)프로그래밍"
             onChange={(e) => {
-              expert(e.target.value);
+              setExpertval(e.target.value);
             }}
+            required
           />
           <span>전문가가 될 것 이다.</span>
         </label>
@@ -32,10 +39,11 @@ export default function MyInfo({ expert, time, input, result }) {
           <span>그래서 앞으로 매일 하루에</span>
           <input
             type="number"
-            placeholder="예)5"
+            placeholder="예) 5"
             onChange={(e) => {
-              time(Math.floor(10000 / e.target.value));
+              setTimeval(e.target.value);
             }}
+            required
           />
           <span>시간씩 훈련할 것이다.</span>
         </label>
